26b498ec81b56bc570db986b62b34c8650901f30
Giovanna Falcão <78114184+gifalcao@users.noreply.github.com>
2021-09-27 11:30:32 -0300

    Create Animal.cs

---
+++ ATV11/Animal.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_Atividade
+{
+    public class Animal
+    {
+        protected virtual void Caracteristicas()
+        {
+            Console.Write("Animais - Caracteristicas");
+        }
+    }
+}
