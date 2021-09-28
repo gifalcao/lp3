be02475630be22a26a63349c67a646a0ae03cbb7
Giovanna Falcão <78114184+gifalcao@users.noreply.github.com>
2021-09-28 12:02:17 -0300

    Update Program.cs

--- a12/Program.cs
+++ a12/Program.cs
@@ -1,13 +1,22 @@
-﻿using System;
+using System;
 
-namespace classes_abstratas
+namespace Atividade_aula12
 {
     class Program
     {
         static void Main(string[] args)
         {
-           Quadrado quad1 = new Quadrado(15.0);
-           Quadrado quad2 = new Quadrado(10.0, "vermelho");
+            Quadrado quadrado1 = new Quadrado(7.0);
+            Quadrado quadrado2 = new Quadrado("Vermelho", 8.0);
+
+            Circulo circulo1 = new Circulo(3.0);
+            Circulo circulo2 = new Circulo("Roxo", 6.0);
+
+            Retangulo retangulo1 = new Retangulo(8.0, 3.0);
+            Retangulo retangulo2 = new Retangulo("Rosa",9.0,6.0);
+            
+            Triangulo triangulo1 = new Triangulo(6.0, 2.0);
+            Triangulo triangulo2 = new Triangulo("Azul", 10.0, 5.0);
         }
     }
 }
