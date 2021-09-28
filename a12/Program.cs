b811c55c03c76d017f8f1076475f9a7dffce53f5
Giovanna Falcão <78114184+gifalcao@users.noreply.github.com>
2021-09-28 11:59:37 -0300

    Add files via upload

---
+++ a12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace classes_abstratas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Quadrado quad1 = new Quadrado(15.0);
+           Quadrado quad2 = new Quadrado(10.0, "vermelho");
+        }
+    }
+}
