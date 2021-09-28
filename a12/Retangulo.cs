cebde0b29e56033d1d21b6eee474f302c87a98e1
Giovanna Falcão <78114184+gifalcao@users.noreply.github.com>
2021-09-28 12:05:02 -0300

    Create Retangulo.cs

---
+++ a12/Retangulo.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_aula12
+{
+    class Retangulo : Forma
+    {
+        private double lado;
+        private double altura;
+
+        public Retangulo(double lado, double altura)
+        {
+            base.setCor("Roxo");
+            this.lado = lado;
+            this.altura = altura;
+            Console.WriteLine("Área do Quadrado: " + this.calculcarArea() + "\n");
+        }
+
+        public Retangulo(string cor, double lado, double altura)
+        {
+            base.setCor(cor);
+            this.lado = lado;
+            this.altura = altura;
+            Console.WriteLine("Área do Quadrado: " + this.calculcarArea() + "\n");
+        }
+        public override double calculcarArea()
+        {
+            return lado * altura;
+        }
+    }
+}
