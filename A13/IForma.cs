e1a952cd604731d0075d7c143195949dd59b9fe0
Giovanna Falcão <78114184+gifalcao@users.noreply.github.com>
2021-09-30 14:26:36 -0300

    Create IForma.cs

---
+++ A13/IForma.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_aula13
+{
+    interface IForma
+    {
+        public double calcularArea();
+    }
+}
